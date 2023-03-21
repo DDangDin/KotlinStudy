e75493de8c2aaf0dcd5c29f4cdd1cc4613195cd1
DDangDin <kcorelec7123@gmail.com>
2023-03-21 21:49:58 +0900

    코틀린 기초 (2장) 정리 끝 (23.03.21)

--- app/src/main/java/com/example/kotlinstudy/study/KotlinBasic2.kt
+++ app/src/main/java/com/example/kotlinstudy/study/KotlinBasic2.kt
@@ -1,5 +1,9 @@
 package com.example.kotlinstudy.study
 
+import java.io.BufferedReader
+import java.io.StringReader
+import java.util.TreeMap
+
 /** 2.3 선택 표현과 처리 (enum, when) **/
 
 /** 2.3.1 enum 클래스 정의 **/
@@ -52,6 +56,77 @@ fun getWarmth(color: Color) = when(color) {
 // SmartCast.kt 파일 확인
 
 
+/** 2.4 이터레이션: while과 for 루프 **/
+// while: 자바와 비슷
+// for: for <item> in <elements> 형태
+
+/** 2.4.2 수에 대한 이터레이션: 범위와 수열 **/
+val oneToTen = 1..10    // 범위, 수열, IntRange
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun useFizzBuzz() {
+    for (i in 1..100) {
+        print(fizzBuzz(i))
+    }
+    println()
+    for (i in 100 downTo 1 step 2) {
+        // !! downTo의 기본 증가값은 -1,
+        // !! step 2를 붙이면 증가값이 절대값 2로 바뀌며 방향은 그대로이므로 -2
+        print(fizzBuzz(i))
+    }
+    // !! 1..100 -> 양 끝 값 포함
+    // !! 1 untile 100 -> 양 끝 값 제외
+}
+
+/** 2.4.3 맵에 대한 이터레이션 **/
+fun mapIteration() {
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.code) // -> c.toInt() == c.code
+        binaryReps[c] = binary  // = 자바의 binaryReps.put(c, binary)
+    }
+
+    for ((letter, binary) in binaryReps) {  // 구조분해
+        println("$letter = $binary")
+    }
+}
+
+fun loopWithIndex() {
+    val list = arrayListOf("10", "11", "1001")
+    for ((index, element) in list.withIndex()) {
+        println("$index: $element")
+    }
+}
+
+/** 2.4.4 in으로 컬렉션이나 범위의 원소 검사 **/
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+fun isNotDigit(c: Char) = c !in '0'..'9'
+fun recognize(c: Char) = when(c) {
+    in '0'..'9' -> "It's a digit!"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter!"
+    else -> "I don't know..."
+}
+
+/** 2.5 코틀린의 예외 처리 **/
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: java.lang.NumberFormatException) {
+        return null
+    } finally {
+        reader.close()
+    }
+}
+// 코틀린의 try 키워드도 if나 when처럼 식이다.
+
 
 
 
@@ -60,4 +135,19 @@ fun main() {
     println(ColorRGB.BLUE.rgb())
     println(getMnemonic(Color.BLUE))
     println(getWarmth(Color.ORANGE))
+
+    useFizzBuzz()
+    println()
+    mapIteration()
+    println()
+    loopWithIndex()
+
+    println(isLetter('q'))
+    println(isNotDigit('x'))
+    println(recognize('8'))
+    println("Kotlin" in "Java".."Scala")
+    println("Kotlin" in setOf("Java", "Scala"))
+
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
 }
--- app/src/main/java/com/example/kotlinstudy/study/SmartCast.kt
+++ app/src/main/java/com/example/kotlinstudy/study/SmartCast.kt
@@ -1,2 +1,53 @@
 package com.example.kotlinstudy.study
 
+/** 2.3.5 스마트 캐스트 **/
+// Expr 인터페이스: 아무 메서드도 선언하지 않으며,
+// 단지 여러 타입의 식 객체를 아우르는 공통 타입 역할만 수행한다.
+
+interface Expr
+class Num(val value: Int): Expr                     // value프로퍼티만 존재하는 단순한 클래스, Expr 구현
+class Sum(val left: Expr, val right: Expr): Expr    // 인자가 Expr 타입이므로 Num이나 다른 Sum이 인자로 올 수 있음
+
+// 자바 스타일
+fun eval(e: Expr): Int {
+    if (e is Num) {         // 코틀린 타입 검사 키워드 -> is (자바의 instanceof와 비슷)
+        val n = e as Num    // 원하는 타입으로 명시적으로 타입 캐스팅, 반드시 val 사용
+        return n.value
+    }
+
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+
+    throw IllegalArgumentException("Unknown expression")
+}
+
+// - 코틀린에서는 프로그래머 대신 컴파일러가 캐스팅을 해줌
+// !! 변수를 is로 타입검사를 하고 나면 해당변수를 검사한 타입으로 사용 가능하게 해줌 -> 스마트 캐스팅
+
+// 코틀린 스타일
+fun evalKot(e: Expr): Int =
+    if (e is Num) {
+        e.value
+    } else if (e is Sum) {
+        eval(e.right) + eval(e.left)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
+    }
+// 코틀린 스타일 (when사용)
+fun evalKotWhen(e: Expr): Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
+
+// 요약 -> 코틀린에서는 is로 타입검사를 하면 캐스팅 할필요가 없다. (스마트 캐스팅)
+
+
+fun main() {
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(evalKot(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(evalKotWhen(Sum(Sum(Num(1), Num(2)), Num(4))))
+}
